297d748312a4e94ebf7453cc50b0373b35296d75
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-07-25 00:44:12 +0530

    fast api started

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import joblib
 import pandas as pd
 from typing import Literal
 from fastapi import FastAPI
+from pydantic import BaseModel,Field
 
 model=joblib.load('Model/Mental_Health_model.pkl')
 top_countries=['Other','India','USA','Canada','Australia','UK','Germany','Mexico','Turkey','France']
@@ -9,3 +10,22 @@ top_countries=['Other','India','USA','Canada','Australia','UK','Germany','Mexico
 app=FastAPI()
 
 
+class StudenData(BaseModel):
+    age   :int=Field(...,ge=10,le=100)
+    gender : Literal['Male','Female']
+    country :str
+    academic_level :Literal['Undergraduate','Graduate','High School']
+    most_used_platform :Literal['Facebook','Linkedin','Instagram','Snapchat','Twitter','Youtube','TikTok','LINE','KakaoTalk','VKontakte','WhatsApp','WeChat']
+    avg_daily_usage_hours=float=Field(...,ge=0,le=24)
+    daily_unlocks : int=Field(...,ge=0)
+    study_hours : float=Field(...,ge=0,le=24)
+    physical_activity_hours:float=Field(...,ge=0,le=24)
+    sleep_hours_per_night:float=Field(...,ge=0,le=24)
+    stress_level:Literal['Medium','Low','Very High','High']
+
+class PredictionResponse(BaseModel):
+    predicted_mental_health_score:float
+
+@app.get('/')
+def greet():
+    return {'Welcome to the Mental Health score Guys'}
